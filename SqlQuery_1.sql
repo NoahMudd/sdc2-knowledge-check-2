1f808728d48787893dd89c4078e427e86528c455
Noah Mudd <noah.mudd@outlook.com>
2023-03-03 16:25:24 -0500

    added SQL query commands

---
+++ SqlQuery_1.sql
@@ -0,0 +1,16 @@
+﻿SELECT c.CustomerName
+FROM Orders o
+INNER JOIN
+Customers c
+ON
+o.CustomerID = c.CustomerID
+WHERE o.OrderID = 10310
+
+
+
+SELECT s.Address 
+FROM Products p
+INNER JOIN
+Suppliers s
+ON p.SupplierID = s.SupplierID
+WHERE p.ProductID = 40
